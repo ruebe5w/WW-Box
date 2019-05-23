a77e003d01ff55412f675269923d72ff11679bac
ruebefuenfw@gmail.com <ghgandalf1#>
2019-05-23 15:35:33 +0200

    Complete Scenario Class

--- wwbox/scenario.py
+++ wwbox/scenario.py
@@ -2,16 +2,25 @@ from configparser import ConfigParser
 
 
 class Scenario:
-    def __init__(self, name: str, author: str, description: str):
+    def __init__(self, name: str, author: str, description: str, story_audio: str):
         self.name = name
         self.author = author
         self.description = description
         self.roles = {}
+        self.story_audio = story_audio
 
     def write_to_file(self):
         """Saves Scenario to config"""
         file = ConfigParser(allow_no_value=True)
-        file['GENERAL'] = {'name': self.name, 'author': self.author, 'description': self.description}
+        file['GENERAL'] = {'name': self.name, 'author': self.author, 'description': self.description,
+                           'story_audio': self.story_audio}
         role_name_array = {}
         for role in self.roles:
             role_name_array.update({role})
+        file['ROLES'] = role_name_array
+
+        file_name = self.name + '.ini'
+        print('Write to \"' + file_name + '\"')
+
+        with open(file_name, 'w') as f:
+            file.write(f)
