4cee07f4a4bc66d6495aaa3f4490c0ec77b73881
ruebefuenfw@gmail.com <ghgandalf1#>
2019-05-24 15:58:20 +0200

    Add tools

--- wwbox/game.py
+++ wwbox/game.py
@@ -1,6 +1,7 @@
 from wwbox.importation import *
 from wwbox.player import Player
 import random
+from wwbox.tools import *
 
 
 class Game:
@@ -8,7 +9,7 @@ class Game:
 
     def __init__(self, id=0, status=0):
         self.id = id
-        self.scenario = Scenario()
+        self.scenario = Scenario()  # TODO
         self.status = status
         self.players = {}
         self.roles = {}
@@ -36,15 +37,55 @@ class Game:
         self.roles.update(import_roles())
 
     def start(self, scenario_name):
+        """Starts a Game"""
         print('Ein neues Spiel wird gestartet!')
         self.status = 1
         print('Scenario \"' + scenario_name + '\" wird geladen...')
         scenarios = import_scenario()
         self.scenario = scenarios[scenario_name]
         self.__role_assignment()
-        self.play_audio(self.scenario.story_audio)  # TODO Threading
         self.generate_order()
+        play_audio(self.scenario.story_audio)  # TODO Threading
+        self.game_routine()
 
+    def game_routine(self):
+
+        def night():  # TODO
+            nonlocal first_night
+            if first_night:
+                print()
+                # 1R
+                first_night = False
+            # PR
+            # KR
+            # AR
+
+        def day():
+            print()
+            # TODO
+            # Dorf erwacht
+
+            # Tode setzen & verkünden
+            # Diskussion
+            # Abstimmung
+            # Tode setzen & verkünden
+
+            # Dorf schläft ein
+
+        first_night = True
+        # 0R (Bürgermeister)
+        # Dorf schläft ein
+        night()
+        while not self.__is_won():
+            day()
+            night()
+        # Gewinner verkünden
+        # Neustarten
+
+    def __is_won(self):
+        # TODO
+        bol = False
+        return bol
 
     def __role_assignment(self):
         player_count = len(self.players)
@@ -58,19 +99,8 @@ class Game:
             self.roles.update({key: self.scenario.roles[key]})
             player = self.players[player_id_array[i]]
             player.set_primary_role(self.roles[key])
-            role_gedöns = ""  # TODO Send Players Role
-            self.send_gui(player, role_gedöns)  # TODO
-
-    def send_gui(self, player, gui_commands):  # TODO static?
-        """Updates Player's GUI"""
-        print()
-        # TODO send information to players
-        # if not death:
-
-    def play_audio(self, audio_file):  # TODO static?
-        """Plays an audio file"""
-        print()
-        # TODO play_audio
+            role_gedons = ""  # TODO Send Players Role
+            send_gui(player, role_gedons)  # TODO
 
     def generate_order(self):
         """Generates the order of toa"""
---
+++ wwbox/tools.py
@@ -0,0 +1,11 @@
+def play_audio(audio_file):  # TODO static?
+    """Plays an audio file"""
+    print()
+    # TODO play_audio
+
+
+def send_gui(player, gui_commands):  # TODO static?
+    """Updates Player's GUI"""
+    print()
+    # TODO send information to players
+    # if not death:
