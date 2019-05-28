cffba555e2db2926bf8a69e2da2561e16c884c42
ruebefuenfw@gmail.com <ghgandalf1#>
2019-05-28 19:04:29 +0200

    Add Gamestart with Gui to test.py

--- wwbox/test.py
+++ wwbox/test.py
@@ -1,24 +1,16 @@
 from wwbox.game import Game
-from wwbox.tools import *
 from wwbox.web import WebThread
 from wwbox.web import instance_dict
-import time
 
 web_thread = WebThread()
 web_thread.start()
 old_instance_dict = {}
-gamestatus_dict = {'status': 0}
+gamestatus_dict = {'status': 0, 'scenario': 'Flohs Kuenstler-WG'}
 game = Game()
 while gamestatus_dict['status'] == 0:
     if old_instance_dict != instance_dict:
-        for player in instance_dict.keys():
-            if player not in game.players:
-                game.new_player()
-for i in range(1, 30):
-    try:
-        for player_key in instance_dict:
-            set_gui(player=player_key, base='info', txt1={'text': 'Hallo ich bin eine autonome Abstimmung.'},
-                    imgPicture='Rückseite.png')
-    except:
-        print('_____________________________________________________________________')
-    time.sleep(2)
+        for player_key in instance_dict.keys():
+            if player_key not in game.players:
+                game.new_player(instance_dict[player_key]['name'], player_key)
+if gamestatus_dict['status'] == 1:
+    game.start(gamestatus_dict['scenario'])
