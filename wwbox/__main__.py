a430c5eec6e272d266a2e60492cb752ed7e31022
ruebe5w <ruebefuenfw@gmail.com>
2019-08-15 20:28:22 +0200

    Make Gameload loop better

--- wwbox/__main__.py
+++ wwbox/__main__.py
@@ -8,12 +8,13 @@ web_thread.start()
 old_instance_dict = {}
 
 game = Game()
-while gamestatus_dict['status'] == 0:
-    if old_instance_dict != instance_dict:
-        for player_key in instance_dict.keys():
-            if player_key not in game.players:
-                game.new_player(instance_dict[player_key]['name'], player_key)
+
 while True:
+    if gamestatus_dict['status'] != 1:
+        if old_instance_dict != instance_dict:
+            for player_key in instance_dict.keys():
+                if player_key not in game.players:
+                    game.new_player(instance_dict[player_key]['name'], player_key)
     if gamestatus_dict['status'] == 5:  # Tutorial
 
         for player in game.players.keys():
